3c73a552aef56c05ac61a253eb3b426a8fc3fcfa
Md Fahim Chowdhury <163567991+fahim-56@users.noreply.github.com>
2024-11-24 23:27:00 +0600

    Create Reflection Both axis.cpp

---
+++ Reflection Both axis.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+using namespace std;
+int main(void)
+{
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"C:\\TC\\BGT");
+
+     cout<<"Enter x1,y1,x2,y2,x3,y3"<<endl;
+    int x1,y1,x2,y2,x3,y3;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+
+    line(x1,y1,x2,y2);
+    line(x1,y1,x3,y3);
+    line(x3,y3,x2,y2);
+
+    y1=-y1+300;
+    y2=-y2+300;
+    y3=-y3+300;
+
+    line(x1,y1,x2,y2);
+    line(x1,y1,x3,y3);
+    line(x3,y3,x2,y2);
+
+    cout<<"Enter x1,y1,x2,y2,x3,y3"<<endl;
+
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+
+    line(x1,y1,x2,y2);
+    line(x1,y1,x3,y3);
+    line(x3,y3,x2,y2);
+
+    x1=-x1+300;
+    x2=-x2+300;
+    x3=-x3+300;
+
+    line(x1,y1,x2,y2);
+    line(x1,y1,x3,y3);
+    line(x3,y3,x2,y2);
+
+
+    getch();
+    closegraph();
+    return 0;
+}
+/*
+100 100
+200 100
+150 50
+
+50 150
+100 100
+100 200
+*/
