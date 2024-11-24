131dea5dc63fe505574bd6f42840d73b152bac6b
Md Fahim Chowdhury <163567991+fahim-56@users.noreply.github.com>
2024-11-24 23:27:55 +0600

    Create Shearing Y axis.cpp

---
+++ Shearing Y axis.cpp
@@ -0,0 +1,41 @@
+
+#include<bits/stdc++.h>
+#include<graphics.h>
+using namespace std;
+int main(void)
+{
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"C:\\TC\\BGT");
+
+    cout<<"Enter x1,y1,x2,y2,x3,y3"<<endl;
+
+    int x1,y1,x2,y2,x3,y3;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+
+    cout<<"Enter shx shy :"<<endl;
+    int shx,shy;
+    cin>>shx>>shy;
+
+    line(x1,y1,x2,y2);
+    line(x1,y1,x3,y3);
+    line(x3,y3,x2,y2);
+
+    y1 = y1 + (shy*x1);
+    y2 = y2 + (shy*x2);
+    y3 = y3 + (shy*x3);
+
+    line(x1,y1,x2,y2);
+    line(x1,y1,x3,y3);
+    line(x3,y3,x2,y2);
+
+    getch();
+    closegraph();
+    return 0;
+}
+/*
+100 100
+50 50
+150 50
+
+2 2
+*/
