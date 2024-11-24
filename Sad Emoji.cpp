142d67ae8da7f56bc5bd4fc82d751882d778bd09
Md Fahim Chowdhury <163567991+fahim-56@users.noreply.github.com>
2024-11-24 23:20:47 +0600

    Create Sad Emoji.cpp

---
+++ Sad Emoji.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+#include<conio.h>
+int main()
+{
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"C:\\TC\\BGT");
+
+
+    //Emoji sad
+    setcolor(YELLOW);
+    circle(200,200,100);
+    circle(170,170,10);
+    circle(230,170,10);
+    arc(200,250,10,180,40);
+
+
+
+
+    getch();
+    closegraph();
+    return 0;
+}
