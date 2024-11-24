b7011a2b1248b7d958c3c19f6ddf37d43ae64fbc
Md Fahim Chowdhury <163567991+fahim-56@users.noreply.github.com>
2024-11-24 23:21:24 +0600

    Create Senti Emoji.cpp

---
+++ Senti Emoji.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+#include<graphics.h>
+#include<conio.h>
+int main()
+{
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,"C:\\TC\\BGT");
+
+
+    //Emoji senti
+    setcolor(YELLOW);
+    circle(200,200,100);
+    circle(170,170,10);
+    circle(230,170,10);
+    arc(200,200,220,320,50);
+
+
+    getch();
+    closegraph();
+    return 0;
+}
